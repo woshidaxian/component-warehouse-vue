6f2fe4340059b65c6f1a15c28aa6d4a4793a8e85
HWG <wghuangzj@sina.com>
2022-08-31 07:19:39 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <header>
+      <div>
+        <span class="title">组件仓库-VUE</span>
+        <input type="text" placeholder="输入关键词搜索..." class="inp-search">
+      </div>
+      <div class="login-btn">登录</div>
+    </header>
     <router-view/>
+    <footer>
+      版权所有All Rights Reserved 
+      <a href="https://github.com/woshidaxian" target="blank">
+        https://github.com/woshidaxian
+      </a>
+      <!-- <span>请联系wghuangzj@sina.com</span> -->
+    </footer>
   </div>
 </template>
 
 <style lang="scss">
+html,body{
+  height: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100%;
+  min-height: 100%;
+  background: rgb(236, 236, 236);
 }
 
 #nav {
@@ -27,6 +43,59 @@
     &.router-link-exact-active {
       color: #42b983;
     }
+  }
+}
+*{
+  padding: 0;
+  margin: 0;
+}
+header{
+  width: calc(100% - 30px);
+  padding: 0 15px;
+  height: 50px;
+  background: #005bb6;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  .title{
+    font-size: 15px;
+    color: #fff;
+    font-family: 黑体;
+    font-weight: bold;
+  }
+  .inp-search{
+    width: 250px;
+    height: 25px;
+    border-radius: 13px;
+    outline: none;
+    border: none;
+    margin-left: 20px;
+    text-indent: 1em;
+  }
+  .login-btn{
+    width: 60px;
+    height: 25px;
+    line-height: 25px;
+    border-radius: 13px;
+    background: #fff;
+    color: #005bb6;
+    cursor: pointer;
+  }
+}
+footer{
+  border-top: 1px solid rgb(218, 218, 218);
+  position: fixed;
+  bottom: 0;
+  padding: 10px 0;
+  width: 100%;
+  color: #8f8f8f;
+  font-size: 12px;
+  background: rgb(236, 236, 236);
+  a{
+    text-decoration: none;
+    color: #005bb699;
   }
 }
 </style>
---
+++ src/views/index.vue
@@ -0,0 +1,150 @@
+<!--  -->
+<template>
+  <div class="page">
+    <div class="list">
+      <div
+        v-for="(item, index) in list"
+        :key="index"
+        class="list-item"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+
+  data () {
+    return {
+      list: [
+        {
+          id: 1,
+          title: '物联弹窗-能耗管理',
+          img: '',
+          time: '2022-08-08 12:12:12',
+          author: 'hwg',
+          download: 100,
+          fileName: 'energy-control.vue',
+          fileUrl: ''
+        },
+        {
+          id: 1,
+          title: '物联弹窗-能耗管理',
+          img: '',
+          time: '2022-08-08 12:12:12',
+          author: 'hwg',
+          download: 100,
+          fileName: 'energy-control.vue',
+          fileUrl: ''
+        },
+        {
+          id: 1,
+          title: '物联弹窗-能耗管理',
+          img: '',
+          time: '2022-08-08 12:12:12',
+          author: 'hwg',
+          download: 100,
+          fileName: 'energy-control.vue',
+          fileUrl: ''
+        },
+        {
+          id: 1,
+          title: '物联弹窗-能耗管理',
+          img: '',
+          time: '2022-08-08 12:12:12',
+          author: 'hwg',
+          download: 100,
+          fileName: 'energy-control.vue',
+          fileUrl: ''
+        },
+        {
+          id: 1,
+          title: '物联弹窗-能耗管理',
+          img: '',
+          time: '2022-08-08 12:12:12',
+          author: 'hwg',
+          download: 100,
+          fileName: 'energy-control.vue',
+          fileUrl: ''
+        },
+        {
+          id: 1,
+          title: '物联弹窗-能耗管理',
+          img: '',
+          time: '2022-08-08 12:12:12',
+          author: 'hwg',
+          download: 100,
+          fileName: 'energy-control.vue',
+          fileUrl: ''
+        },
+        {
+          id: 1,
+          title: '物联弹窗-能耗管理',
+          img: '',
+          time: '2022-08-08 12:12:12',
+          author: 'hwg',
+          download: 100,
+          fileName: 'energy-control.vue',
+          fileUrl: ''
+        },
+        {
+          id: 1,
+          title: '物联弹窗-能耗管理',
+          img: '',
+          time: '2022-08-08 12:12:12',
+          author: 'hwg',
+          download: 100,
+          fileName: 'energy-control.vue',
+          fileUrl: ''
+        },
+        {
+          id: 1,
+          title: '物联弹窗-能耗管理',
+          img: '',
+          time: '2022-08-08 12:12:12',
+          author: 'hwg',
+          download: 100,
+          fileName: 'energy-control.vue',
+          fileUrl: ''
+        }
+      ],
+      isLoading: false
+    }
+  },
+
+  computed: {},
+
+  mounted() {},
+
+  methods: {},
+
+  beforeDestroy() {}
+}
+
+</script>
+<style lang='scss' scoped>
+.page{
+  padding-bottom: 40px;
+  padding-top: 70px;
+  padding-left: 20px;
+  padding-right: 20px;
+  min-height: 100%;
+  .list{
+    width: 100%;
+    min-height: 100%;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    .list-item{
+      width: 24%;
+      height: 400px;
+      background: #fff;
+      border-radius: 16px;
+      margin-bottom: 20px;
+    }
+  }
+}
+</style>
